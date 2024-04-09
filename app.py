f8e9fce9f7fbd30b5d20933c0be0da586edc7327
Hardy Namakhwa <66388667+Hard265@users.noreply.github.com>
2024-04-09 19:10:59 +0000

    update

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+socketio = SocketIO(app)
+
+
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@socketio.on('message')
+def handle_message(message):
+    print('Received message:', message)
+    # Broadcast the received message to all connected clients
+    emit('message', message, broadcast=True)
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
+
+
+@app.route('/')
+def index():
+    # Render the HTML template named "index.html" located in the "templates" folder
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True, log_output=False)
